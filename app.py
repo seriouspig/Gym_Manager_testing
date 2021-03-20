3baa8cb0d4959ff2015a8e9cd62c4bfa9faca40c
Piotr <pigryko@yahoo.com>
2021-03-20 22:07:22 +0000

    View and create activities added

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template 
 
 from controllers.member_controller import members_blueprint
-
+from controllers.activity_controller import activities_blueprint
 
 
 app = Flask(__name__)
 
 app.register_blueprint(members_blueprint) 
+app.register_blueprint(activities_blueprint) 
+
 
 
 
